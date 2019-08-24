f2723eab4ab769d25120be0bdc5c2a7129bbb48d
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-24 01:25:02 -0400

    Create hero and banner components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>hello from home page</div>;
+  return (
+    <Hero hero="defaultHero">
+      <Banner
+        title="Rumeet's Rich Rooms"
+        subtitle="Deluxe rooms starting at $299 per night"
+      >
+        <Link to="/rooms" className="btn-primary">
+          Our Rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
