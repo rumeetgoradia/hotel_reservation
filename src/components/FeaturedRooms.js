6cd1e3afde39c04dc00e970771908ca3a72744af
Rumeet Goradia <rumeet.goradia@gmail.com>
2019-08-28 22:48:29 -0400

    Incorporate logic for featured rooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,12 +4,8 @@ import { RoomContext } from "../context";
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
-    // console.log(value);
-    return (
-      <div>
-        {greeting} {name} from featuredrooms{" "}
-      </div>
-    );
+    const { featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    return <div>from featuredrooms </div>;
   }
 }
